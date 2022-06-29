7c4e3e6967ac1f7412613908dfa70d2f5b5eea61
Xuan Tang <txstc55@gmail.com>
2022-06-29 22:17:02 +0800

    Add comments for ComputeKernel.cpp

--- src/ComputeKernel.cpp
+++ src/ComputeKernel.cpp
@@ -40,10 +40,14 @@ Kernel::Kernel(const vector<Symbolic>& expressions, const int _simdWidth) : simd
         unsigned char status = 0;
 
         bool visit() {
+            // this will check if we have visited all immediate children
+            // since x contains all same strucutres
+            // checking one is enough
             return childCounter++ == x.front().numChilds();
         }
 
         unsigned int numChilds() const {
+            // returns the number of immediate children
             return x.empty() ? 0 : x.front().numChilds();
         }
 
@@ -52,7 +56,10 @@ Kernel::Kernel(const vector<Symbolic>& expressions, const int _simdWidth) : simd
         }
 
         bool isValid() {
-
+            // check if every symbolic is of same structure
+            // this is not a strict check
+            // since we only check the immediate number of children
+            // but that's enough
             for (auto& y : x) {
                 if (y.op() != op()) return false;
                 if (y.numChilds() != numChilds()) return false;
@@ -63,12 +70,17 @@ Kernel::Kernel(const vector<Symbolic>& expressions, const int _simdWidth) : simd
 
         void init() {
             if (!x.empty()) {
+                // this will give us a total hash
+                // of all elements in x
+                // by hashing all of the algebraic hashes together
                 h = hashSymbolic(x);
                 if (x.front().op() == VAR) status = LEAF_VAR;
                 else if (x.front().op() == CONST) status = LEAF_CONST;
             }
         }
 
+        // return the list of variables
+        // <data id, matrix id>
         vector<array<int, 2>> getVariables() {
             if (op() == VAR) {
                 const int n = x.size();
@@ -87,6 +99,7 @@ Kernel::Kernel(const vector<Symbolic>& expressions, const int _simdWidth) : simd
             return {};
         }
 
+        // return all the constants from x
         vector<double> getConstants() {
             if (op() == CONST) {
                 const int n = x.size();
@@ -139,6 +152,9 @@ Kernel::Kernel(const vector<Symbolic>& expressions, const int _simdWidth) : simd
         }
     };
 
+    // the stack initially will only have one StackData
+    // which has its x set to expressions
+    // x is a vector of symbolic
     vector<StackData> stack{ expressions };
     vector<Symbolic> tStack;
     map<hash_t, int> leafMap;
@@ -146,30 +162,61 @@ Kernel::Kernel(const vector<Symbolic>& expressions, const int _simdWidth) : simd
     while (!stack.empty()) {
 
         auto& curr = stack.back();
+        // check if all of them are of the same structure
         assert(curr.isValid());
 
         if (curr.visit()) {
             // all childs have been traversed
+            // now we want to create a new symbolic
+            // that has the children that points to outputVariabletable
+            // or variableTable, or constantTable
             Symbolic txpr(curr.op(), vector<Symbolic>(tStack.end() - curr.numChilds(), tStack.end()));
+            // remove the children and push itself
             tStack.erase(tStack.end() - curr.numChilds(), tStack.end());
             tStack.push_back(txpr);
             if (xprMap.find(curr.h) == xprMap.end()) xprMap[curr.h] = txpr;
             stack.pop_back();
         } else {
             // analyze the current child
+            // the -1 is because when calling .visit() function
+            // we have childCounter++
+            // this StackData will have nth child of curr
+            // where n = curr.childCounter - 1
+            // imagine you have a list of
+            //    *        *        *
+            // X1  X2,  X3  X4,  X5   X6
+            // (X1, X2 ...) don't have to be variables
+            // now you will instead have a list of
+            // [X1, X3, X5], and in next iteration, [X2, X4, X6]
             StackData data(curr.x, curr.childCounter - 1);
 
             auto h = data.h;
+            // if somehow we have saved this result before
+            // xprMap is a map from hash to symbolic
             auto it = xprMap.find(h);
 
             if (it != xprMap.end()) { // child is already present
                 tStack.push_back(xprMap[h]);
             } else if (data.op() == VAR) { // child is a variable
                 auto values = data.getVariables();
+                // childCounter == 1 when visiting left child
                 if (curr.op() == ASSIGN && curr.childCounter == 1) {
+                    // matrix id -2 for outputVariables
+                    // this will only occur if we have a tree like this:
+                    //        =
+                    //    x1     +
+                    //        x2   x3
+                    // then we can say x1 is an output variable
                     tStack.push_back(Symbolic(outputVariableTable.size(), -2));
                     outputVariableTable.push_back(values);
                 } else {
+                    // else, visiting right child
+                    // or curr.op is not assign
+                    // for example
+                    //     +
+                    // x1     x2
+                    // if x1 or x2 is an VAR tree, then we enter this loop
+                    // matrix id -3 for variableTables
                     tStack.push_back(Symbolic(variableTable.size(), -3));
                     variableTable.push_back(values);
                 }
@@ -179,6 +226,7 @@ Kernel::Kernel(const vector<Symbolic>& expressions, const int _simdWidth) : simd
                 if (values.size() == 1) {
                     tStack.push_back(Symbolic(values.front()));
                 } else {
+                    // matrix id -4 for constant values
                     tStack.push_back(Symbolic(constantTable.size(), -4));
                     constantTable.push_back(move(values));
                 }
@@ -189,6 +237,7 @@ Kernel::Kernel(const vector<Symbolic>& expressions, const int _simdWidth) : simd
         }
     }
 
+    // to fit in a simd vector, we add extra terms at the end
     if (simdWidth > 1 && n > simdWidth && n % simdWidth) {
         int pad = simdWidth - n % simdWidth;
 
@@ -210,32 +259,54 @@ Kernel::Kernel(const vector<Symbolic>& expressions, const int _simdWidth) : simd
     }
 
     assert(tStack.size() == 1);
+    // this will give us the modified top level node
+    // this node will represent every symbolic in input
+    // with the leaf node accessible in one of the tables
+    // outputVariatable, variableTable, or constantTable
     templateExpression = tStack.front();
     optimizeTemplateExpression();
 }
 
 void Kernel::optimizeTemplateExpression() {
-
+    // TX: Philipp, removeConstantExpressions is not doing anything now
     templateExpression = removeConstantExpressions(templateExpression);
     //  templateExpression = simplify(templateExpression);
     templateExpression = removeConstantExpressions(templateExpression);
+    // TX: Philipp, referenceRedundant is not doing anything now
     templateExpression = referenceRedundant(templateExpression);
 
     vector<Symbolic> vec{ templateExpression };
+    // LOCAL_VAR is 253
+    // the first decompose, in ComputeUnit.cpp,
+    // is to find duplicated expressions at a global scope
+    // this one, however, is to find duplicated expressions at local level
+    // note, a duplicated expression means two expressions
+    // having the same algebraic hash
     auto blocks = decompose(vec, 0, LOCAL_VAR, false);
 
     // simplify blocks
     for (auto& b : blocks) {
+        // simplify does the a list of things
+        // that basically takes out common multiplier
+        // or remove some constants
+        // or change the operation to something else
+        // but as the name indicates
+        // it simplifies the structure
+        // without creating new sub-structures
         b.x = simplify(b.x);
     }
 
     // relabel assigned variables to defines
+    // Remember, we just decomposed the expression
+    // to detect symbolics that repeatedly show up in the tree
     for (auto& b : blocks) {
         if (b.x.op() == ASSIGN && b.x[0].variable()[1] == LOCAL_VAR)
             b.x = Symbolic(DEFINE, b.x[0], b.x[1]);
     }
 
     // sort blocks
+    // we want each unordered_set to contain
+    // the parent of block indexed at c
     vector<unordered_set<int>> graph(blocks.size());
     for (int i = 0; i < blocks.size(); ++i) {
         for (auto& c : blocks[i].childs) {
@@ -246,6 +317,8 @@ void Kernel::optimizeTemplateExpression() {
     auto order = topologicalOrder(graph);
     vector<Symbolic> kernelExpressions;
     for (int i : order) kernelExpressions.push_back((blocks[i].x));
+    // templateExpression contains a block of expressions
+    // that is already sorted by topological order
     templateExpression = Symbolic(BLOCK, kernelExpressions);
 }
 
@@ -254,8 +327,12 @@ Symbolic Kernel::getTemplateExpression() {
 }
 
 std::string Kernel::generateKernelCode() {
+    // when entering here, templateExpression will have op = BLOCK
+    // where its children are expressions that are topologically sorted
     return generateCode(templateExpression, [&](const array<int, 2>& a) {
         if (a[1] == -2) {
+            // format replace % with the input data
+            // a[1] = -2 means its an output variable
             switch (outputVariablePackLength) {
                 case 0:
                     return format("y[% + i]", n * a[0]);
@@ -267,7 +344,7 @@ std::string Kernel::generateKernelCode() {
                     return format("y[(i * % + %) * % + j]", outputVariableTable.size(), a[0], outputVariablePackLength);
             }
         } else if (a[1] == -3) {
-
+            // a[1] == -3 means its an input variable
             auto va = variableAccessData[a[0]];
 
             switch (variablePackLength) {
@@ -292,6 +369,8 @@ std::string Kernel::generateKernelCode() {
                     return format("c[(i * % + %) * % + j]", constantTable.size(), a[0], constantsPackLength);
             }
         } else if (a[1] == LOCAL_VAR) {
+            // a[1] == LOCAL_VAR
+            // means it's a repated variable in a local kernel
             return format("r%", a[0]);
         }
 
@@ -341,6 +420,7 @@ void Kernel::findCorrelatedVariables() {
         bool flag = true;
         vals.push_back(vars[0][1]);
 
+        // compute the variable access difference
         for (auto v : vars) {
             if (v[1] != vars[0][1]) {
                 flag = false;
@@ -349,18 +429,28 @@ void Kernel::findCorrelatedVariables() {
 
             vals.push_back(v[0] - vars[0][0]);
         }
+        // vals now contain a pattern, with 0th value being the input group id
+        // and the rest being the pattern
 
+        // variableAccessData is vector<VariableAccessData> 
+        // VariableAccessData is struct{int baseVariableId; int offset;}
         if (!flag || vars[0][1] == GLOBAL_INTERMEDIATE || vars[0][1] == EXPLICIT_INTERMEDIATE) {
             variableAccessData.push_back({ i, 0 });
         } else {
             auto h = hash(vals);
             auto it = varMap.find(h);
             if (it == varMap.end()) {
+                // if this patten is not found
+                // we store this pattern with index in variableAccessData
                 varMap[h] = i;
                 variableAccessData.push_back({ i, 0 });
             } else {
+                // we have found this pattern
+                // then push the index of previous pattern, and find the gap
+                // between this group and the previously found group
                 variableAccessData.push_back({ it->second, vars[0][0] - variableTable[it->second][0][0] });
 
+                // making sure everything works
                 for (int i = 0; i < vars.size(); ++i) {
                     assert(vars[i][1] == variableTable[it->second][i][1]);
                     assert(vars[i][0] - variableTable[it->second][i][0] == variableAccessData.back().offset);
@@ -384,6 +474,8 @@ void Kernel::findCorrelatedVariables() {
             va.baseVariableId = i;
             ++i;
         } else {
+            // don't push to the variableTable if we have a correlated variable sequence
+            // instead, record the correlated data in variableAccessData for later access
             va.baseVariableId = it->second;
         }
     }
